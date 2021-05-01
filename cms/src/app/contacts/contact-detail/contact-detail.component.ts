e39610e8d3c07e210eb7539000c5984360867951
soldochris <soldevilla.christian@gmail.com>
2021-04-30 19:57:11 -0500

    week2 assignment ready

---
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
+
+@Component({
+  selector: 'app-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
+})
+export class ContactDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
